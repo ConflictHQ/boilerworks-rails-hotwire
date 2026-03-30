16879197e1fd2e47f9c730f02cab47d9d8ceac57
Leo Mata <1504549+ragelink@users.noreply.github.com>
2026-03-29 23:29:46 -0600

    fix: remove before_save callback conflicting with AR optimistic locking

--- app/models/concerns/versionable.rb
+++ app/models/concerns/versionable.rb
@@ -1,13 +1,4 @@
 module Versionable
   extend ActiveSupport::Concern
-
-  included do
-    before_save :increment_lock_version, if: :changed?
-  end
-
-  private
-
-  def increment_lock_version
-    self.lock_version = (lock_version || 0) + 1 if persisted?
-  end
+  # lock_version column is automatically managed by ActiveRecord's optimistic locking
 end
